d7c0e305dc90d2438703afaf47c960b0f1cc9205
remiscarlet <remilia@scarl.et>
2025-06-17 22:46:52 -0700

    Add body reserve level to hotspot_ring_view

--- src/ekaine/alembic/versions/2025-06-17_33ea35e672c1_update_views.py
+++ src/ekaine/alembic/versions/2025-06-17_33ea35e672c1_update_views.py
@@ -1,12 +1,11 @@
 """Update views
 
-Revision ID: be5cda8acaec
-Revises: c730527d0c09
-Create Date: 2025-06-15 00:27:08.956709
+Revision ID: 33ea35e672c1
+Revises: 2086129b5566
+Create Date: 2025-06-17 16:49:05.488484
 
 Updates:
-- derived.station_commodities_view v1 -> v2
-- derived.acquisition_routes v1 -> v2
+- derived.hotspot_ring_view v1 -> v2
 """
 
 from typing import Sequence
@@ -17,19 +16,22 @@ from ekaine.common.constants import SQL_DIR
 
 views_sql_dir = SQL_DIR / "views"
 
+
 # revision identifiers, used by Alembic.
-revision: str = "be5cda8acaec"
-down_revision: str | None = "c730527d0c09"
+revision: str = "33ea35e672c1"
+down_revision: str | None = "2086129b5566"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.execute("drop view if exists derived.acquisition_routes")
-    op.execute("drop view if exists derived.station_commodities_view")
+    # Need to drop acquisition_routes first as it depends on hotspot_ring_view
 
-    with open(views_sql_dir / "derived_station_commodities_view_v2.sql") as f:
+    op.execute("drop view if exists derived.acquisition_routes")
+    op.execute("drop view if exists derived.hotspot_ring_view")
+
+    with open(views_sql_dir / "derived_hotspot_ring_view_v2.sql") as f:
         op.execute(f.read())
     with open(views_sql_dir / "derived_acquisition_routes_v2.sql") as f:
         op.execute(f.read())
@@ -40,7 +42,7 @@ def downgrade() -> None:
     op.execute("drop view if exists derived.acquisition_routes")
     op.execute("drop view if exists derived.station_commodities_view")
 
-    with open(views_sql_dir / "archives" / "derived_station_commodities_view_v1.sql") as f:
+    with open(views_sql_dir / "archives" / "derived_hotspot_ring_view_v1.sql") as f:
         op.execute(f.read())
-    with open(views_sql_dir / "archives" / "derived_acquisition_routes_v1.sql") as f:
+    with open(views_sql_dir / "derived_acquisition_routes_v2.sql") as f:
         op.execute(f.read())
